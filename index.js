daeb465d1136b9cde2361573f57966aee9643d5e
Isabella Mighell <ikm4dm@virginia.edu>
2021-08-12 11:12:25 -0400

    commit tree c7cc4f4979a3757dbdb114e7d428aac31d3a7651

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(record){
+    const result = record.find(record => record.result === "W")
+    if( typeof result === "object"){
+        return result.year
+    }
+    else{
+        return undefined
+    }
+}
+    
